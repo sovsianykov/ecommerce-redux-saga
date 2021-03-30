ab1420edf31c5424088e596bcaf08d73bd181a2d
”sovsianykov” <swebproger@gmail.com>
2021-03-30 16:59:59 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+ import React from 'react';
+import './default.scss'
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
+    <div className='App'>
+      <Header/>
 
     </div>
   );
