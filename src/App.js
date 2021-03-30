bda316eab8116d736096f64446f9b20573579a76
”sovsianykov” <swebproger@gmail.com>
2021-03-30 17:43:14 +0300

    first 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
  import React from 'react';
 import './default.scss'
 import Header from "./components/Header";
+ import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div className='App'>
       <Header/>
-
+      <Homepage/>
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,15 @@
  import  React from 'react';
 import './styles.scss'
+ import logo from '../../assets/logo.jpg'
 
 const Header = () => {
     return (
         <header className='header'>
-
+            <div className="wrap">
+                <div className="logo">
+                    <img src={logo} alt="logo"/>
+                </div>
+            </div>
         </header>
     )
 }
