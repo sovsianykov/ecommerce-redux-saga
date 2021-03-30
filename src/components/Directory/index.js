2be1f78bffeb2a94b905086a54e090784aa5e5bb
”sovsianykov” <swebproger@gmail.com>
2021-03-30 18:40:03 +0300

    first 2

--- src/components/Directory/index.js
+++ src/components/Directory/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import ShopMen from './../../assets/shopMens.jpg'
+import ShopWomen from './../../assets/shopWomens.jpg'
 
 const Directory = () => {
     return (
-        <div>
+        <div className='directory'>
+           <div className="wrap">
+               <div className='item' style={{background : `url(${ShopWomen})`}}>
+               </div>
+               <div className='item' style={{background : `url(${ShopMen})`}}>
+               </div>
+           </div>
 
         </div>
     )
--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import Directory from "../../components/Directory";
+import Directory from "../../components/Directory/index";
+
 
 const Homepage =( ) =>{
     return (
         <section>
+
          <Directory/>
         </section>
 
